37a6fa44e19fa3f84b5396730dc81b7583c0bc71
Cristian Reyes <cristian.rba@hotmail.com>
2021-06-24 12:12:04 -0400

    cambios a smart & dumb components, short input

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { ProductsComponent } from './components/products/products.component';
+import { ProductsContainer } from './container/products/products.container';
 
 import { ProductRoutingModule } from './product-routing.module';
-import { SharedModule } from './../shared/shared.module';
-import { MaterialModule } from './../material/material.module';
+import { SharedModule } from '@shared/shared.module';
+import { MaterialModule } from '@material/material.module';
 
 @NgModule({
-  declarations: [ProductComponent, ProductDetailComponent, ProductsComponent],
+  declarations: [ProductComponent, ProductDetailComponent, ProductsContainer],
   imports: [CommonModule, SharedModule, ProductRoutingModule, MaterialModule],
   exports: [SharedModule, ProductComponent]
 })
